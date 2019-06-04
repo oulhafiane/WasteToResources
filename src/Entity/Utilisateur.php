f8d7ee073ce394314aac70ca4a560ee27d6afc5f
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2019-06-04 15:59:10 +0000

    Adding User interface to Utilisateur

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -3,18 +3,35 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\MappedSuperclass
  */
-abstract class Utilisateur
+abstract class Utilisateur implements UserInterface
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue(strategy="UUID")
-     * @ORM\Column(type="guid")
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    protected $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    protected $password;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -29,17 +46,17 @@ abstract class Utilisateur
     /**
      * @ORM\Column(type="string", length=255)
      */
-    protected $email;
+    protected $ville;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    protected $ville;
+    protected $addresse;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    protected $adresse;
+    protected $pays;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
@@ -83,6 +100,79 @@ abstract class Utilisateur
         return $this->id;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
     public function getPrenom(): ?string
     {
         return $this->prenom;
@@ -107,18 +197,6 @@ abstract class Utilisateur
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
     public function getVille(): ?string
     {
         return $this->ville;
@@ -131,14 +209,26 @@ abstract class Utilisateur
         return $this;
     }
 
-    public function getAdresse(): ?string
+    public function getAddresse(): ?string
     {
-        return $this->adresse;
+        return $this->addresse;
     }
 
-    public function setAdresse(string $adresse): self
+    public function setAddresse(string $addresse): self
     {
-        $this->adresse = $adresse;
+        $this->addresse = $addresse;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?string $pays): self
+    {
+        $this->pays = $pays;
 
         return $this;
     }
